abb57db1c448011e523447e942978b6a70fe2705
luzi82 <luzi82@gmail.com>
2018-01-19 19:14:21 +0800

    complete random_text_sample

--- python3/random_text_sample.py
+++ python3/random_text_sample.py
@@ -1,8 +1,11 @@
 import freetype
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import string
+import shutil
+import random
+import cv2
 
 CREATE_FONT_INFO_TEXT_USED_DEFAULT = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
@@ -37,13 +40,18 @@ def create_text(text, font_info, fill=True, stroke=False, stroke_radius=0):
 
     # First pass to compute bbox
     width = 0
+    width2 = 0
     previous = 0
     for c in text:
         face.load_char(c)
         bitmap = slot.bitmap
         kerning = face.get_kerning(previous, c)
+        width2 = width + bitmap.width + (kerning.x >> 6)
         width += (slot.advance.x >> 6) + (kerning.x >> 6)
         previous = c
+
+    width = max(width,width2)
+    width += 1
 
     width  += stroke_radius*2
     Z = np.zeros((height,width), dtype=np.int)
@@ -97,24 +105,82 @@ def create_text(text, font_info, fill=True, stroke=False, stroke_radius=0):
     
     return Z
 
+def get_width(text, font_info, fill=True, stroke=False, stroke_radius=0):
+
+    face = font_info['font_face']
+    height = font_info['height']
+    baseline = font_info['baseline']
+
+    slot = face.glyph
+
+    width = 0
+    previous = 0
+    for c in text:
+        face.load_char(c)
+        bitmap = slot.bitmap
+        kerning = face.get_kerning(previous, c)
+        width += (slot.advance.x >> 6) + (kerning.x >> 6)
+        previous = c
+
+    width  += stroke_radius*2
+    
+    return width
+
+def draw(dest_hwc, src_c, z_hw):
+    assert(dest_hwc.shape[0] == z_hw.shape[0])
+    assert(dest_hwc.shape[1] == z_hw.shape[1])
+    assert(dest_hwc.shape[2] == 3)
+    assert(src_c.shape[0] == 3)
+    
+    h = dest_hwc.shape[0]
+    w = dest_hwc.shape[1]
+    c = 3
+
+    z_chw = np.reshape(z_hw,(1,h,w))
+    z_chw = np.repeat(z_chw,c,axis=0)
+    z_hwc = np.transpose(z_chw,(1,2,0))
+
+    dz255_hwc = dest_hwc * (255-z_hwc)
+    
+    src_hwc = np.reshape(src_c,(1,1,c))
+    src_hwc = np.repeat(src_hwc,w,axis=1)
+    src_hwc = np.repeat(src_hwc,h,axis=0)
+    
+    sz255_hwc = src_hwc * z_hwc
+    
+    dest_hwc[:,:,:] = (dz255_hwc+sz255_hwc)/255
+
+def reset_dir(out_dir):
+    shutil.rmtree(out_dir,ignore_errors=True)
+    os.makedirs(out_dir)
+
 if __name__ == '__main__':
 
-    char_set = CREATE_FONT_INFO_TEXT_USED_DEFAULT
+    CHAR_SET = CREATE_FONT_INFO_TEXT_USED_DEFAULT
+    CHAR_COUNT_MAX = 10
+    OUT_PATH = 'output'
+    STROKE_RADIUS_MIN = 1
+    STROKE_RADIUS_MAX = 4
+
+    reset_dir(OUT_PATH)
 
     face = freetype.Face(os.path.join('font_set','Vera.ttf'))
     face.set_char_size( 48*64 )
-    font_info = create_font_info(face)
-    height_max = font_info['height']
+    font_info = create_font_info(face,text_used=CHAR_SET)
+    inner_height_max = font_info['height']
+    print('inner_height_max = {}'.format(inner_height_max))
+
+    #inner_width_max = max([get_width(c*CHAR_COUNT_MAX,font_info) for c in CHAR_SET])
 
     for _ in range(100):
-        text_len = random.randint(1,20)
-        text = ''.join(random.choice(char_set) for _ in range(text_len))
-        font_size = random.randint(36*64,48*64)
+        text_len = random.randint(1,CHAR_COUNT_MAX)
+        text = ''.join(random.choice(CHAR_SET) for _ in range(text_len))
+        font_size = random.randint(24*64,48*64)
         draw_type = random.choice(['FILL', 'STROKE_DOWN', 'STROKE_UP'])
         if draw_type=='FILL':
             stroke_radius = 0
         else:
-            stroke_radius = random.randint(1,4)
+            stroke_radius = random.randint(STROKE_RADIUS_MIN,STROKE_RADIUS_MAX)
         if draw_type=='FILL':
             bg_color = np.random.rand(3)
             while True:
@@ -131,9 +197,37 @@ if __name__ == '__main__':
                 if diff > 0.25:
                     break
 
-        font_info = create_font_info(face)
+        print('text={}, font_size={}, stroke_radius={}',text,font_size,stroke_radius)
+
+        face.set_char_size( font_size )
+        font_info = create_font_info(face,text_used=CHAR_SET)
         fill_z = create_text(text, font_info, fill=True, stroke=False, stroke_radius=stroke_radius)
         if draw_type!='FILL':
             stroke_z = create_text(text, font_info, fill=False, stroke=True, stroke_radius=stroke_radius)
 
-        // blah...
+        height = inner_height_max + stroke_radius*2
+        width  = fill_z.shape[1]
+        zh = fill_z.shape[0]
+        y_freedom = height-zh
+        assert(y_freedom>=0)
+        y_offset = random.randint(0,y_freedom)
+        
+        img_hwc = bg_color.reshape((1,1,3))
+        img_hwc = np.repeat(img_hwc,width,axis=1)
+        img_hwc = np.repeat(img_hwc,height,axis=0)
+        
+        if draw_type == 'STROKE_DOWN':
+            draw(img_hwc[y_offset:y_offset+zh,:,:], stroke_color, stroke_z)
+        draw(img_hwc[y_offset:y_offset+zh,:,:], fill_color, fill_z)
+        if draw_type == 'STROKE_UP':
+            draw(img_hwc[y_offset:y_offset+zh,:,:], stroke_color, stroke_z)
+        
+        img_hwc = img_hwc*255
+        img_hwc = np.floor(img_hwc)
+        img_hwc = np.minimum(img_hwc,255)
+        img_hwc = np.maximum(img_hwc,0)
+        print(np.max(img_hwc))
+        
+        output_fn = os.path.join(OUT_PATH,'{}.png'.format(text))
+        cv2.imwrite(output_fn,img_hwc)
+        
